1a8b0c17156eb11bb0f579bd1e78cfc2d090e203
Brendan Cooney <101305939+b-cooney@users.noreply.github.com>
2024-11-27 10:47:07 -0500

    init
    add files

---
+++ src/components/Tagline.tsx
@@ -0,0 +1,7 @@
+export const Tagline = () => {
+    return (
+      <p className="text-2xl md:text-3xl text-gray-600 max-w-2xl text-center leading-normal font-light px-6">
+        Join the world's first on-chain mathematics competition for young minds. Showcase your skills, win prizes, and become part of a global community!
+      </p>
+    );
+  };
