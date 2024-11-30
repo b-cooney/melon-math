009366e24d6ae8983d310a2a208a5e4877040142
Brendan Cooney <101305939+b-cooney@users.noreply.github.com>
2024-11-30 00:47:35 -0500

    Update Links.tsx

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,7 +5,7 @@ export const Links = () => {
   return (
     <div className="flex flex-col gap-4">
       <a
-        href="#"
+        href="https://melonmath.notion.site/Melon-Math-International-Mathematics-Competition-1493cd7f9d58809781cfddfde66ee06d?pvs=74"
         className="group flex items-center gap-2 px-6 py-3 text-lg text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-all duration-200"
       >
         Learn more about Melon Math
